a7a7ce4dd166ad4aa13bcc245f8243748894ece4
carlos-hl <carlos-hl@segplan.go.gov.br>
2018-10-18 16:04:31 -0300

    #24732 Criação do Projeto

---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ShoppingCartService } from './shopping-cart-services'
+
+@Component({
+  selector: 'mt-shopping-cart',
+  templateUrl: './shopping-cart.component.html'
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  ngOnInit() {
+  }
+
+  items(): any[] {
+    return this.shoppingCartService.items
+  }
+
+  total(): number {
+    return this.shoppingCartService.total();
+  }
+
+}
