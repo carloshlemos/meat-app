dd5b029d063eaf25e587f215c55dee707a647fb2
carlos-hl <carlos-hl@segplan.go.gov.br>
2018-10-19 15:24:17 -0300

    #24732 Implementação do Module Shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart-services";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { OrderService } from "../order/order.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -12,5 +15,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
               CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {
-
+  static forRoot(): ModuleWithProviders {
+      return {
+          ngModule: SharedModule,
+          providers: [ShoppingCartService, RestaurantsService, OrderService]
+      }
+  }
 }
