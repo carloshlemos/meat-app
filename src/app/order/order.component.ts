30154cdb420a393d2fa1042bc45d79e1e5d74384
carlos-hl <carlos-hl@segplan.go.gov.br>
2018-10-19 09:41:53 -0300

    #24732 Criação do componente radio

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RadionOption } from '../shared/radio/radion-option.model'
+
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadionOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão Débito', value: 'DEB'},
+    {label: 'Cartão Refeição', value: 'REF'}
+  ]
 
   constructor() { }
 
